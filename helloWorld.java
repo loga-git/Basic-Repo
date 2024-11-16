021daa8cbd194285727c0fecf1e5081b7d2c7f69
loga-git <logavananlogi@gmail.com>
2024-11-16 22:19:18 +0530

    This is the first helloWorld commit

---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+class helloWorld {
+    public static void main(String[] args)
+    {
+        String s = "Hi, this is loga from Zoho corporation. I am working as an SRE.";
+        System.out.println(s);
+    }
+}
